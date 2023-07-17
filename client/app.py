7e9f9bd221b7ece7a248edc4e9e16a5287a936ec
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-07-17 00:51:42 +0000

    pushで移動

--- client/app.py
+++ client/app.py
@@ -1,33 +1,63 @@
 import streamlit as st
 import requests as r
+import influxdb_client, os
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
 
-st.title('hello world!')
+token = os.environ.get("INFLUXDB_TOKEN")
+org = "lp-cloud"
+url = "http://10.0.0.2:8086"
 
-with st.form(key='login_form'):
-    username = st.text_input('ユーザー名')
-    password = st.text_input('パスワード', type='password')
-    submit_button = st.form_submit_button(label='ログイン')
+write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+bucket="congestion"
+
+write_api = write_client.write_api(write_options=SYNCHRONOUS)
+
+
+st.title('manage page')
+
+with st.form(key='form'):
+    username = st.text_input('表示させる文章')
+    submit_button = st.form_submit_button(label='送信')
 
 if submit_button:
-    response = r.get(f'https://api.launchpencil.f5.si/zikanwari/?user={username}&pass={password}')
+    response = r.get(f'https://api.launchpencil.f5.si/zikanwari/?user={username}')
     if response.status_code == 200:
+
+        st.info(response.text)
         
-        a = response.text.split(',')
-
-        if a[0] == 'エラー':
-            if str(a[1]).startswith("Access denied for user"):
-                st.error('認証に失敗しました。ユーザー名またはパスワードが間違っているか設定されていません。')
-            else:
-                st.error(a[1])
-            st.stop()
-
-        table_data = [[0 for j in range(5)] for i in range(7)]
-        table_data[1][2] = 5
-
-        for i in range(0, 35):
-            table_data[i//5][i%5] = a[i]
-
-        st.table(table_data)
     else:
         # エラーが発生した場合はエラーメッセージを表示する
         st.error(response.status_code)
+
+if st.button('とても混んでいる(100%)'):
+    point = (
+    Point("congestion")
+    .tag("tagname1", "tagvalue1")
+    .field("field1", 100)
+    )
+    write_api.write(bucket=bucket, org="lp-cloud", record=point)
+
+if st.button('混んでいる(75%)'):
+    point = (
+    Point("congestion")
+    .tag("tagname1", "tagvalue1")
+    .field("field1", 75)
+    )
+    write_api.write(bucket=bucket, org="lp-cloud", record=point)
+
+if st.button('普通(50%)'):
+    point = (
+    Point("congestion")
+    .tag("tagname1", "tagvalue1")
+    .field("field1", 50)
+    )
+    write_api.write(bucket=bucket, org="lp-cloud", record=point)
+
+if st.button('空いている(0%)'):
+    point = (
+    Point("congestion")
+    .tag("tagname1", "tagvalue1")
+    .field("field1", 0)
+    )
+    write_api.write(bucket=bucket, org="lp-cloud", record=point)
