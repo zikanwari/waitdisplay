d4172ffb733917ffcc8ac7a087c05c1250b9f99e
launchpencil <launchpencil@gmail.com>
2023-07-18 12:37:36 +0900

    とりあえずpush

--- client/app.py
+++ client/app.py
@@ -4,12 +4,13 @@ import influxdb_client, os
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-token = os.environ.get("INFLUXDB_TOKEN")
+#token = os.environ.get("INFLUXDB_TOKEN")
+token = ''
 org = "lp-cloud"
 url = "http://10.0.0.2:8086"
 
 write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-bucket="congestion"
+bucket="zikanwari"
 
 write_api = write_client.write_api(write_options=SYNCHRONOUS)
 
@@ -17,47 +18,47 @@ write_api = write_client.write_api(write_options=SYNCHRONOUS)
 st.title('manage page')
 
 with st.form(key='form'):
-    username = st.text_input('表示させる文章')
+    message_text = st.text_input('表示させる文章')
     submit_button = st.form_submit_button(label='送信')
 
 if submit_button:
-    response = r.get(f'https://api.launchpencil.f5.si/zikanwari/?user={username}')
-    if response.status_code == 200:
-
-        st.info(response.text)
-        
-    else:
-        # エラーが発生した場合はエラーメッセージを表示する
-        st.error(response.status_code)
+    point = (
+    Point("congestion")
+    .field("message", message_text + ",")
+    )
+    write_api.write(bucket=bucket, org="lp-cloud", record=point)
 
 if st.button('とても混んでいる(100%)'):
     point = (
     Point("congestion")
-    .tag("tagname1", "tagvalue1")
-    .field("field1", 100)
+    .field("percentage", 100)
     )
     write_api.write(bucket=bucket, org="lp-cloud", record=point)
 
 if st.button('混んでいる(75%)'):
     point = (
     Point("congestion")
-    .tag("tagname1", "tagvalue1")
-    .field("field1", 75)
+    .field("percentage", 75)
     )
     write_api.write(bucket=bucket, org="lp-cloud", record=point)
 
 if st.button('普通(50%)'):
     point = (
     Point("congestion")
-    .tag("tagname1", "tagvalue1")
-    .field("field1", 50)
+    .field("percentage", 50)
     )
     write_api.write(bucket=bucket, org="lp-cloud", record=point)
 
-if st.button('空いている(0%)'):
+if st.button('空いている(25%)'):
     point = (
     Point("congestion")
-    .tag("tagname1", "tagvalue1")
-    .field("field1", 0)
+    .field("percentage", 25)
+    )
+    write_api.write(bucket=bucket, org="lp-cloud", record=point)
+
+if st.button('整備中(0%)'):
+    point = (
+    Point("congestion")
+    .field("percentage", 0)
     )
     write_api.write(bucket=bucket, org="lp-cloud", record=point)
---
+++ server/code/index.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const {InfluxDB, Point} = require('@influxdata/influxdb-client')
+
+const token = ''
+const url = 'http://10.0.0.2:8086'
+
+let org = `lp-cloud`
+let bucket = `zikanwari`
+
+const client = new InfluxDB({url, token})
+
+queryClient = client.getQueryApi(org)
+
+fluxQuery = `from(bucket: "zikanwari")
+|> range(start: -60m)
+|> filter(fn: (r) => r["_measurement"] == "congestion")
+|> last()`
+
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+
+    queryClient.queryRows(fluxQuery, {
+    next: (row, tableMeta) => {
+        const tableObject = tableMeta.toObject(row)
+        console.log(tableObject._value)
+        res.write(String(tableObject._value))
+
+        
+    },
+    error: (error) => {
+        console.error('\nえららー', error)
+        res.end()
+    },
+    complete: () => {
+        res.end()
+    },
+    })
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+  });
