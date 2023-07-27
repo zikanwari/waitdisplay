573fe972233d9f462d4bf806f193721fca6dfad0
launchpencil <launchpencil@gmail.com>
2023-07-27 18:27:36 +0900

    試しに

--- show/html/show/files/index.js
+++ show/html/show/files/index.js
@@ -7,7 +7,8 @@ fetch(`https://api.launchpencil.f5.si/waitdisplay/`, {
         document.getElementById('message').innerHTML = a[0]
         //document.getElementById('message').innerText = 'ここにテキストを入力\nhogehuga'
 
-        syncstatus(a[1]);
+        //syncstatus(a[1]);
+        syncstatus(75);
         
 })
 .catch(error => {
