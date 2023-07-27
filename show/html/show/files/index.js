ba21667dbe5e337ad0f51e746f5ee09e9e839137
launchpencil <launchpencil@gmail.com>
2023-07-27 18:41:56 +0900

    まじかよ…

--- show/html/show/files/index.js
+++ show/html/show/files/index.js
@@ -17,29 +17,23 @@ fetch(`https://api.launchpencil.f5.si/waitdisplay/`, {
 
 
 function syncstatus(percentage) {
-    switch (percentage) {
-        case percentage = '25':
-            document.getElementById('status').src = 'images/25.png'
-            document.getElementById('main').style.backgroundColor = '#5b9bd5'
-            break;
-        case percentage = '100':
-            document.getElementById('status').src = 'images/100.png'
-            document.getElementById('main').style.backgroundColor = '#954eca'
-            break;
-        case percentage = '75':
-            document.getElementById('status').src = 'images/75.png'
-            document.getElementById('main').style.backgroundColor = '#ff5b5b'
-            break;
-        case percentage = '50':
-            document.getElementById('status').src = 'images/50.png'
-            document.getElementById('main').style.backgroundColor = '#ED7D31'
-            break;
-        default:
-            document.getElementById('status').style.display = 'none'
-            document.getElementById('message').style.width = '100%'
-            document.getElementById('message').style.paddingRight = '0'
-            document.getElementById('main').style.backgroundColor = '#A5A5A5'
-            break;
-    }
+    if (percentage === '100') {
+        document.getElementById('status').src = 'images/100.png';
+        document.getElementById('main').style.backgroundColor = '#954eca';
+    } else if (percentage === '75') {
+        document.getElementById('status').src = 'images/75.png';
+        document.getElementById('main').style.backgroundColor = '#ff5b5b';
+    } else if (percentage === '50') {
+        document.getElementById('status').src = 'images/50.png';
+        document.getElementById('main').style.backgroundColor = '#ED7D31';
+    } else if (percentage === '25') {
+        document.getElementById('status').src = 'images/25.png';
+        document.getElementById('main').style.backgroundColor = '#5b9bd5';
+    } else {
+        document.getElementById('status').style.display = 'none';
+        document.getElementById('message').style.width = '100%';
+        document.getElementById('message').style.paddingRight = '0';
+        document.getElementById('main').style.backgroundColor = '#A5A5A5';
+    }    
     
 }
