b56c7781941a6cb978a59438826d09a7d0462a64
launchpencil <launchpencil@gmail.com>
2023-07-27 17:52:23 +0900

    でけた

--- show/html/show/files/index.js
+++ show/html/show/files/index.js
@@ -4,7 +4,7 @@ fetch(`https://api.launchpencil.f5.si/waitdisplay/`, {
 .then(response => response.text())
 .then(data => {
         a = data.split(',');
-        document.getElementById('message').innerText = a[0]
+        document.getElementById('message').innerHTML = a[0]
         //document.getElementById('message').innerText = 'ここにテキストを入力\nhogehuga'
 
         syncstatus(a[1]);
@@ -19,24 +19,25 @@ function syncstatus(percentage) {
     switch (percentage) {
         case percentage = '100':
             document.getElementById('status').src = 'images/100.png'
-            document.getElementById('main').style.backgroundColor = '#ED7D31'
+            document.getElementById('main').style.backgroundColor = '#954eca'
             break;
         case percentage = '75':
             document.getElementById('status').src = 'images/75.png'
-            document.getElementById('main').style.backgroundColor = '#000000'
+            document.getElementById('main').style.backgroundColor = '#ff5b5b'
             break;
         case percentage = '50':
             document.getElementById('status').src = 'images/50.png'
-            document.getElementById('main').style.backgroundColor = '#000000'
+            document.getElementById('main').style.backgroundColor = '#ED7D31'
             break;
         case percentage = '25':
             document.getElementById('status').src = 'images/25.png'
-            document.getElementById('main').style.backgroundColor = '#000000'
+            document.getElementById('main').style.backgroundColor = '#5b9bd5'
             break;
     
         default:
             document.getElementById('status').style.display = 'none'
-            document.getElementById('message').style.textAlign = 'center'
+            document.getElementById('message').style.width = '100%'
+            document.getElementById('message').style.paddingRight = '0'
             document.getElementById('main').style.backgroundColor = '#A5A5A5'
             break;
     }
